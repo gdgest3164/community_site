6dec82e045b01da8b5970d92aea0de3a294646c7
gdgest3164 <rlaehdgs12@naver.com>
2023-11-25 20:15:42 +0900

    add. 상점 UI/UX 추가 끗

---
+++ app/routes/shop.success.tsx
@@ -0,0 +1,22 @@
+import { Alert, Box, Space, Title } from "@mantine/core";
+import { IconCheck } from "@tabler/icons-react";
+
+export default function Shop() {
+  return (
+    <Box
+      style={{
+        padding: "0 50px",
+        paddingTop: "50px",
+        width: "calc(100% - 100px)",
+        maxWidth: "1100px",
+        margin: "0 auto",
+      }}
+    >
+      <Title>포인트 구매완료</Title>
+      <Space h="xl" />
+      <Alert icon={<IconCheck size="1rem" />} title="결제 완료">
+        포인트 구매가 완료되었습니다.
+      </Alert>
+    </Box>
+  );
+}
